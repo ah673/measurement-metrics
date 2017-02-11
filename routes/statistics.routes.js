fadf44a6dd4a6876edd5028e50aea299527e9ed2
Amy Ho <ah673@cornell.edu>
2017-02-11 17:57:00 -0500

    Refactor * Move statistics route logic from Measurements data store to statistics route

--- routes/statistics.routes.js
+++ routes/statistics.routes.js
@@ -33,10 +33,92 @@ var StatisticRoutes = (function () {
             }
         }
 
-        measurements.getStatistics(
-            metric, stat, startDate
-            , endDate, done);
+        calculateStatistics(metric, stat, startDate, endDate, done);
+    }
 
+    function calculateStatistics (metrics, stats, from, to, done) {
+        const measurementsInRange = measurements.getValuesInRange(from, to);
+        if (!Array.isArray(metrics)) {
+            metrics = [metrics];
+        }
+
+        if (!Array.isArray(stats)) {
+            stats = [stats];
+        }
+
+        let statisticsForMetrics = [];
+
+        for (let metric of metrics) {
+            for (let stat of stats) {
+                try {
+                    let value = calculateStatForMetric(stat, metric, measurementsInRange);
+
+                    if (value) {
+                        statisticsForMetrics.push({
+                            metric: metric,
+                            stat: stat,
+                            value: value
+                        });
+                    }
+                } catch (err) {
+                    return done(err);
+                }
+            }
+        }
+        return done(null, statisticsForMetrics);
+    }
+
+    function calculateStatForMetric (stat, metric, measurements) {
+        switch (stat.toLowerCase()) {
+            case 'min':
+                return findMinForMetric(measurements, metric);
+            case 'max':
+                return findMaxForMetric(measurements, metric);
+            case 'average':
+                return findAverageForMetric(measurements, metric);
+
+            default:
+                throw new Error('metric not supported');
+        }
+    }
+
+    function findMinForMetric (measurements, metric) {
+        let value = null;
+        measurements.forEach( (measurement) => {
+            if (measurement[metric]) {
+                if (!value) {
+                    value = measurement[metric];
+                }
+                value = Math.min(value, measurement[metric]);
+            }
+        });
+        return value;
+    }
+
+    function findMaxForMetric (measurements, metric) {
+        let value = null;
+        measurements.forEach( (measurement) => {
+            if (measurement[metric]) {
+                if (!value) {
+                    value = measurement[metric];
+                }
+                value = Math.max(value, measurement[metric]);
+            }
+        });
+        return value;
+    }
+
+    function findAverageForMetric (measurements, metric) {
+        let value = 0;
+        let measurementsWithMetric = 0;
+        measurements.forEach( (measurement) => {
+            if (measurement[metric]) {
+                value += measurement[metric];
+                measurementsWithMetric++;
+            }
+        });
+        const average = value/measurementsWithMetric;
+        return isNaN(average) ? null : average;
     }
 
     return {
